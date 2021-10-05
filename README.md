1fd9de15f215a843edc3a5f3c8dbc4edc12919bb
HarrisWong <firefangj20@gmail.com>
2021-10-05 14:18:09 +0800

    add first demo

---
+++ README.md
@@ -0,0 +1,26 @@
+# tron-web3
+
+
+
+A php interface for interacting with the Tron blockchain.
+
+# Install
+
+```
+composer require harriswong/tron-web3 dev-master
+```
+
+Or you can add this line in composer.json
+
+```
+"harriswong/tron-web3": "dev-master"
+```
+
+
+
+# API
+
+Todo.
+
+# License
+MIT
---
+++ src/tools/NodeClient.php
@@ -0,0 +1,45 @@
+<?php
+namespace Web3\tools;
+
+use GuzzleHttp\Client;
+
+class NodeClient{
+  protected $client;
+
+  static function web3($fullNode){
+    return new self($fullNode);
+  }
+
+  function __construct($uri){
+    $opts = [
+      'base_uri' => $uri
+    ];
+    $this->client = new Client($opts);
+  }
+
+  function post($api,$payload=[]){
+    $opts = [
+      'json' => $payload
+    ];
+    $rsp = $this->client->post($api,$opts);
+    return $this->handle($rsp);
+  }
+
+  function get($api,$query=[]){
+    $opts = [
+      'query' => $query
+    ];
+    $rsp = $this->client->get($api,$opts);
+    return $this->handle($rsp);
+  }
+
+  function handle($rsp){
+    $content = $rsp->getBody();
+    //echo $content . PHP_EOL;
+    return json_decode($content);
+  }
+
+  function version(){
+    return '1.0.0';
+  }
+}
